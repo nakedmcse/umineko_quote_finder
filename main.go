3087b67b59e030ba9bb147be4f9214f1540a7768
VictoriqueMoe <loli@victorique.moe>
2026-01-28 23:18:38 +0000

    add logging

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/filesystem"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 //go:embed static/*
@@ -17,6 +18,11 @@ var staticFiles embed.FS
 
 func main() {
 	app := fiber.New()
+
+	app.Use(logger.New(logger.Config{
+		Format:     "${time} | ${status} | ${latency} | ${ip} | ${method} ${path} ${queryParams}\n",
+		TimeFormat: "2006-01-02 15:04:05",
+	}))
 
 	quoteService := quote.NewService()
 	service := controllers.NewService(quoteService)
